52b45d040553cb58cad6a1c4185c34a628d19ae8
Obegg <13978119+Obegg@users.noreply.github.com>
2022-02-21 13:56:30 +0200

    Add support for https://sukebei.nyaa.si
    A clone version of the original nyaasi.py with a few modifications. It's working for me. Also - might wanna add it to Unofficial search plugins page ( https://github.com/qbittorrent/search-plugins/wiki/Unofficial-search-plugins ). Feel free to change or fix if I missed something. Fixes https://github.com/MadeOfMagicAndWires/qBit-plugins/issues/6

--- engines/sukebeinyaasi.py
+++ engines/sukebeinyaasi.py
@@ -29,11 +29,11 @@ except:
     pass
 
 
-class nyaasi(object):
+class sukebeinyaasi(object):
     """Class used by qBittorrent to search for torrents"""
 
-    url = 'https://nyaa.si'
-    name = 'Nyaa.si'
+    url = 'https://sukebei.nyaa.si'
+    name = 'Sukebei.Nyaa.si'
     # defines which search categories are supported by this search engine
     # and their corresponding id. Possible categories are:
     # 'all', 'movies', 'tv', 'music', 'games', 'anime', 'software', 'pictures',
@@ -48,7 +48,7 @@ class nyaasi(object):
             'tv': '4_0',
             'movies': '4_0'}
 
-    class NyaasiParser(HTMLParser):
+    class SukebeiNyaasiParser(HTMLParser):
         """ Parses Nyaa.si browse page for search resand prints them"""
         def __init__(self, res, url):
             try:
@@ -139,7 +139,7 @@ class nyaasi(object):
 
         hits = []
         page = 1
-        parser = self.NyaasiParser(hits, self.url)
+        parser = self.SukebeiNyaasiParser(hits, self.url)
         while True:
             res = retrieve_url(url + "&p={}".format(page))
             parser.feed(res)
